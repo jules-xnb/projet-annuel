e8d6ce044808b383b46332c80eaa2374e6747bd8
Jules <julesbertrand279@gmail.com>
2021-06-27 14:00:50 +0200

    server: add login/signup + auth

---
+++ server/models/UserModels.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+const uniqueValidator = require ('mongoose-unique-validator')
+
+const userSchema = new mongoose.Schema({
+    address:{
+        type: String,
+        required: true, 
+        unique: true
+    },
+    password: {
+        type: String, 
+        required: true
+    },
+    actualBalance: { 
+        type: Number, 
+        required: true
+    },
+    totalBalance: { 
+        type: Number, 
+        required: true
+    }
+})
+
+userSchema.plugin(uniqueValidator)
+
+module.exports = mongoose.model('mytable', userSchema)
