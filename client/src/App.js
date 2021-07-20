41f30294b3dfc4eec8f1c6eb92c9fc2e2f35c333
Jules <julesbertrand279@gmail.com>
2021-07-20 07:47:40 +0200

    ajout inscription / connexion

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ import Bids from './Component/Pages/Bids'
 import Account from './Component/Pages/Account'
 import Web3 from 'web3'
 import { abi } from './ABI/abi'
-
+import axios from 'axios'
 
 
 class App extends React.Component {
@@ -18,14 +18,17 @@ class App extends React.Component {
     displayBids : true,
     displayHP : true,
     showCanvas : false,
+
     title : "Connexion",
     passwd : "password", 
 
     addressContract: "0x939E52397527b709193B7Eb3A7E249f9803C60dE", 
     userAddress: null, 
+    userToken: null, 
     contract : null, 
+    
 
-    connected : false, 
+    messageInscription : null, 
   }
 
   web3 = new Web3(window.ethereum)
@@ -58,24 +61,41 @@ class App extends React.Component {
       await window.ethereum.enable()
       await this.web3.eth.getAccounts()
           .then(e => {
-            console.log(e[0])
             this.setState({userAddress:e[0]})
           })
       let newcontract = await new this.web3.eth.Contract(abi, this.state.addressContract)
       this.setState({contract: newcontract})
+      console.log(this.state.userAddress)
     }
     //console.log('connection metamask')
   }
 
-  // connect = async () => {
-  //   if (this.state.title === "Inscription"){
-  //     const article = {  };
-  //     const response = await axios.post('https://reqres.in/api/articles', article);
-  //     this.setState({ articleId: response.data.id });
-  //   } else {
-      
-  //   }
-  // }
+  connect = async () => {
+    if (this.state.passwd && this.state.userAddress){
+      if (this.state.title === "Inscription"){
+          const req = {
+            address : this.state.userAddress,
+            password : this.state.passwd
+          };
+          const res = await axios.post('http://localhost:4000/user/signup', req);
+          if (res.status === 201){
+            this.setState({messageInscription: res.data.message + " Veuillez vous connecter"})
+          }
+      } else {
+          const req = {
+            address : this.state.userAddress,
+            password : this.state.passwd
+          };
+          const res = await axios.post('http://localhost:4000/user/login', req)
+          if (res.status === 200){
+            this.setState({userToken: res.data.token, messageInscription: "Connexion réussie"})
+            console.log(this.state.userToken)
+          }
+      }
+    } else {
+      this.setState({messageInscription: "Connexion à Metamask et mot de passe requis"})
+    }
+  }
 
   render(){
     return (
@@ -103,10 +123,12 @@ class App extends React.Component {
 
                         <div className="forms">
                             <h1>{this.state.title}</h1>
+                            <div><button className="connectMetaMask" onClick={() => this.loadWeb3()}>Connexion avec metamask</button></div>
+                            <div>{ this.state.userAddress }</div>
                             <label for="password" onChange={(e) => this.setState({password : e.target.value})}>Mot de passe : </label>
                             <input type="text" className="password" onChange={(e) => this.setState({passwd : e.target.value})}/>
                             <input type="button" value="Submit" onClick={() => this.connect()}/>
-                            <button className="connectMetaMask" onClick={() => this.loadWeb3()}>Connexion avec metamask</button>
+                            <div>{ this.state.messageInscription }</div>
                         </div>
                     </div>
                 </div>
