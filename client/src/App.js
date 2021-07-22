0e1ecf15da280642c90174c23d995d859eab58d0
Jules <julesbertrand279@gmail.com>
2021-07-22 20:32:48 +0200

    ajout deposit/withdraw

--- client/src/App.js
+++ client/src/App.js
@@ -19,9 +19,12 @@ class App extends React.Component {
     showCanvas : false,
 
     title : "Connexion",
-    passwd : "password", 
+    passwd : "password",
 
-    addressContract: "0x939E52397527b709193B7Eb3A7E249f9803C60dE", 
+    privateKeyAdmin: "bcf1f68eac9b08b1e938079e4750f90b468d729b74366be99aac1ba16a1404da",
+    publicKeyAdmin: "0x88352aDC0c6Fa220d6D5e5c797ACC5344f43ebD3",
+    //chainURL: 'https://data-seed-prebsc-1-s1.binance.org:8545',
+    addressContract: "0xaba3B865A8b03f9636be1e1F47bc20248e6bad05", 
     userAddress: null, 
     userToken: null, 
     contract : null, 
@@ -29,15 +32,22 @@ class App extends React.Component {
 
     actualBalance: 0,
     totalBalance: 0,
+    amountDepositWithdraw: 0, 
+    resDepositWithdraw: null, 
+    resDepositWithdraw2: null,
+    resDepositWithdraw3: null,
+    resDepositWithdraw4: null,
 
+    buttonInscription : "Inscription/Connexion",
     messageInscription : null, 
+
+
   }
 
   web3 = new Web3(window.ethereum)
+ 
 
-
-  // ############################################ Connexion MetaMask ############################################
-       
+  // ############################################ Connexion MetaMask ############################################       
 
   loadWeb3 = async () =>{
     if (window.ethereum) {
@@ -48,7 +58,7 @@ class App extends React.Component {
           })
       let newcontract = await new this.web3.eth.Contract(abi, this.state.addressContract)
       this.setState({contract: newcontract})
-      console.log(this.state.userAddress)
+      //console.log(this.state.userAddress)
     }
     //console.log('connection metamask')
   }
@@ -56,7 +66,7 @@ class App extends React.Component {
 
   // ############################################ Connexion / Inscription ############################################
 
-  connect = async () => {
+  connect = () => {
     if (this.state.passwd && this.state.userAddress){
       if (this.state.title === "Inscription"){
           const req = {
@@ -67,26 +77,158 @@ class App extends React.Component {
             totalBalance : this.state.totalBalance
 
           };
-          const res = await axios.post('http://localhost:4000/user/signup', req);
-          if (res.status === 201){
-            this.setState({messageInscription: res.data.message + " Veuillez vous connecter"})
-          }
+          axios.post('http://localhost:4000/user/signup', req)
+          .then(
+            res => {
+              this.setState({messageInscription: res.data.message + " Veuillez vous connecter"})
+            })
+          .catch(
+            () => { this.setState({messageInscription: "Erreur, un utilisateur existe déja pour cette adresse"})
+          })
       } else {
           const req = {
             address : this.state.userAddress,
             password : this.state.passwd,
             actualBalance : this.state.actualBalance,
             totalBalance : this.state.totalBalance
-
           };
-          const res = await axios.post('http://localhost:4000/user/login', req)
-          if (res.status === 200){
-            this.setState({userToken: res.data.token, messageInscription: "Connexion réussie"})
-            console.log(this.state.userToken)
-          }
+          axios.post('http://localhost:4000/user/login', req)
+          .then( res => {
+              this.setState({userToken: res.data.token, messageInscription: "Connexion réussie", buttonInscription: "Connecté"})
+              axios.post("http://localhost:4000/user/user", { address : this.state.userAddress})
+              .then(res2 => {
+              //console.log(res)
+                this.setState({
+                  actualBalance : res2.data[0].actualBalance, 
+                  totalBalance : res2.data[0].totalBalance,
+                })
+              })
+          })
+          .catch(() => { this.setState({messageInscription: "Erreur, utilisateur inconnu ou mot de passe incorrect"})})
       }
     } else {
       this.setState({messageInscription: "Connexion à Metamask et mot de passe requis"})
+    }
+  }
+
+  deposit = async () => {
+    this.setState({ 
+      resDepositWithdraw1 : null, 
+      resDepositWithdraw2 : null,
+      resDepositWithdraw3 : null,
+      resDepositWithdraw4 : null,
+    })
+    if (this.state.userAddress && this.state.userToken && this.state.amountDepositWithdraw > 0){
+      let res = await this.state.contract.methods.deposit(this.state.amountDepositWithdraw).send({
+        from: this.state.userAddress,
+        to: this.state.addressContract
+      })
+      this.setState({ resDepositWithdraw1 : 'Dépot sur le smart-contract, en attente de confirmation :  ' + res.transactionHash })
+
+      let web3 = new Web3('https://data-seed-prebsc-1-s1.binance.org:8545')
+      res = await this.state.contract.methods.getWaitingDepot(this.state.userAddress).call()
+      this.setState({ resDepositWithdraw2 : res + " tokens ont été déposés sur le contrat, transfert vers le dépot en cours" })
+
+      let gasPrice = await web3.eth.getGasPrice()
+      let gasEstimate = await this.state.contract.methods.transferToDepot(this.state.userAddress)
+      .estimateGas({ from: this.state.publicKeyAdmin })
+      let encoded = await this.state.contract.methods.transferToDepot(this.state.userAddress).encodeABI()
+      let tx = {
+          to: this.state.addressContract, 
+          data : encoded,
+          gasPrice, 
+          gas: gasEstimate
+      }
+      let signedtx = await web3.eth.accounts.signTransaction(tx, this.state.privateKeyAdmin)
+      let res2 = await web3.eth.sendSignedTransaction(signedtx.rawTransaction)
+      this.setState({ resDepositWithdraw3 : " Dépot sur la plateforme confirmé, mise à jour de vos balances : " + res2.transactionHash })
+      
+      var ac = this.state.actualBalance * 1 + res * 1
+      var tb = this.state.totalBalance * 1 + res * 1
+
+      let data = JSON.stringify({
+        "address" : this.state.userAddress, 
+        "actualBalance" : ac,
+        "totalBalance" : tb
+      })
+      let config = {
+        method: 'put',
+        url: 'http://localhost:4000/user/update',
+        headers: { 
+          'Authorization': 'Bearer ' + this.state.userToken, 
+          'Content-Type': 'application/json'
+        },
+        data : data
+      };
+      axios(config)
+      .then(() => {
+        this.setState({
+          actualBalance : ac, 
+          totalBalance : tb,
+        })
+        this.setState({ resDepositWithdraw4 : "Balances mises à jour" })
+      })
+      
+    } else {
+      this.setState({ resDepositWithdraw1 : "Vous n'êtes pas connecté ou montant incorrect" })
+    }
+  }
+
+  withdraw = async () => {
+    this.setState({ 
+      resDepositWithdraw1 : null, 
+      resDepositWithdraw2 : null,
+      resDepositWithdraw3 : null,
+      resDepositWithdraw4 : null,
+    })
+    if (this.state.userAddress && this.state.userToken 
+      && this.state.amountDepositWithdraw > 0 && this.state.amountDepositWithdraw <= this.state.actualBalance ){
+        this.setState({ resDepositWithdraw2 : "Demande de retrait en cours de traitement" })
+      let web3 = new Web3('https://data-seed-prebsc-1-s1.binance.org:8545')
+      let gasPrice = await web3.eth.getGasPrice()
+      let gasEstimate = await this.state.contract.methods.withdraw(this.state.userAddress, this.state.amountDepositWithdraw)
+      .estimateGas({ from: this.state.publicKeyAdmin })
+      let encoded = await this.state.contract.methods.withdraw(this.state.userAddress, this.state.amountDepositWithdraw).encodeABI()
+      let tx = {
+        to: this.state.addressContract, 
+        data : encoded,
+        gasPrice, 
+        gas: gasEstimate
+    }
+    let signedtx = await web3.eth.accounts.signTransaction(tx, this.state.privateKeyAdmin)
+    let res2 = await web3.eth.sendSignedTransaction(signedtx.rawTransaction)
+    this.setState({ resDepositWithdraw3 : " Retrait sur la plateforme confirmé, mise à jour de vos balances : " + res2.transactionHash })
+
+    var ac = this.state.actualBalance * 1 - this.state.amountDepositWithdraw * 1
+    var tb = this.state.totalBalance * 1 - this.state.amountDepositWithdraw * 1
+
+    let data = JSON.stringify({
+      "address" : this.state.userAddress, 
+      "actualBalance" : ac,
+      "totalBalance" : tb
+    })
+
+    let config = {
+      method: 'put',
+      url: 'http://localhost:4000/user/update',
+      headers: { 
+        'Authorization': 'Bearer ' + this.state.userToken, 
+        'Content-Type': 'application/json'
+      },
+      data : data
+    };
+
+    axios(config)
+    .then(() => {
+      this.setState({
+        actualBalance : ac, 
+        totalBalance : tb,
+      })
+      this.setState({ resDepositWithdraw4 : "Balances mises à jour" })
+    })
+
+    } else {
+      this.setState({ resDepositWithdraw1 : "Vous n'êtes pas connecté ou montant incorrect" })
     }
   }
 
@@ -97,7 +239,7 @@ class App extends React.Component {
         actualBalance : this.state.actualBalance,
         totalBalance : this.state.totalBalance
       };
-      const res = await axios.get('http://localhost:4000/bid/getBids', req)
+      await axios.get('http://localhost:4000/bid/bids', req)
       // if (res.status === 200){
       //   this.setState({userToken: res.data.token, messageInscription: "Connexion réussie"})
       //   console.log(this.state.userToken)
@@ -138,7 +280,7 @@ class App extends React.Component {
           
           <div className="connect">
                 <div className="itemlast connection">
-                        <button className="button connexion" onClick={() => this.setState({showCanvas : true})}>Connexion</button>
+                        <button className="button connexion" onClick={() => this.setState({showCanvas : true})}>{ this.state.buttonInscription }</button>
                 </div>
                 <div className="canvas" style={{display : this.state.showCanvas ? 'block' : 'none'}}>
                     <div className="background" onClick={() => this.setState({showCanvas : false})}></div>
@@ -171,16 +313,22 @@ class App extends React.Component {
           actualBalance = {this.state.actualBalance}
           totalBalance = {this.state.totalBalance}
         />
+        <input type="text" onChange={e => this.setState({ amountDepositWithdraw: e.target.value })}/>
+        <button onClick={()=> this.deposit()}>Deposit</button>
+        <button onClick={()=> this.withdraw()}>Withdraw</button>
+        <div>{ this.state.resDepositWithdraw1 }</div>
+        <div>{ this.state.resDepositWithdraw2 }</div>
+        <div>{ this.state.resDepositWithdraw3 }</div>
+        <div>{ this.state.resDepositWithdraw4 }</div>
         <div className="componentBid" style={{display : this.state.displayBids ? 'block' : 'none', backgroundColor : "green"}}>
           <div className="divBidFlex">
             <h1>Les enchères</h1>
             <div></div>
-            <button>🔄 Relaod</button>
+            <button>🔄 Reload</button>
           </div>
           <Bids 
             disp = {this.state.displayBids}
           />
-
         </div>
       </div>
     );
@@ -188,3 +336,8 @@ class App extends React.Component {
 }
 
 export default App;
+
+
+
+
+
